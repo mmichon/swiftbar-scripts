e68ff91fd0aa169721674652a696c790a1b504b1
Mike Michon <mmichon@google.com>
2025-09-26 17:17:56 -0700

    Switched to a free weather API provider.

--- weather.15m.py
+++ weather.15m.py
@@ -16,25 +16,13 @@ from urllib.error import URLError
 from random import randint
 import datetime
 import os
-from config import api_key
 
 location_name = "{0}".format(os.getenv('VAR_LOCATION')).replace(" ", "%20")
 
 units = 'imperial'  # kelvin, metric, imperial
-lang = 'en'
-
 def get_wx():
-    if api_key == "":
-        return False
-
     try:
-        daily_wx = json.load(urlopen(f'http://api.openweathermap.org/data/2.5/forecast/daily?q={location_name}'
-                                     f'&units={units}&lang={lang}&appid={api_key}&v={str(randint(0, 100))})))
-        location = str(daily_wx['city']['id'])
-        wx = json.load(
-            urlopen(
-                'http://api.openweathermap.org/data/2.5/weather?id=' + location + '&units=' + units + '&lang=' + lang + '&appid=' + api_key + "&v=" + str(
-                    randint(0, 100))))
+        wx = json.load(urlopen('https://wttr.in/{0}?format=j1'.format(location_name)))
     except URLError:
         return False
 
@@ -47,21 +35,21 @@ def get_wx():
 
     try:
         daily_forecast = []
-        for day in daily_wx['list']:
-            daily_forecast.append({'id': day['weather'][0]['id'],
-                                   'datetime': datetime.datetime.fromtimestamp(day['dt']),
-                                   'max': str(int(round(day['temp']['max']))),
-                                   'min': str(int(round(day['temp']['min'])))
-                                  })
+        for day in wx['weather']:
+            daily_forecast.append({'id': day['hourly'][0]['weatherCode'],
+                                   'datetime': datetime.datetime.strptime(day['date'], '%Y-%m-%d'),
+                                   'max': str(int(round(float(day['maxtempF'])))) if units == 'imperial' else str(int(round(float(day['maxtempC'])))),
+                                   'min': str(int(round(float(day['mintempF'])))) if units == 'imperial' else str(int(round(float(day['mintempC'])))),
+                                   })
         weather_data = {
-            'temperature': str(int(round(wx['main']['temp']))),
-            'condition': str(wx['weather'][0]['description']),
-            'id': wx['weather'][0]['id'],
-            'city': wx['name'],
+            'temperature': str(int(round(float(wx['current_condition'][0]['temp_F'])))) if units == 'imperial' else str(int(round(float(wx['current_condition'][0]['temp_C'])))),
+            'condition': str(wx['current_condition'][0]['weatherDesc'][0]['value']),
+            'id': int(wx['current_condition'][0]['weatherCode']),
+            'city': wx['nearest_area'][0]['areaName'][0]['value'],
             'unit': '°' + unit,
             'daily_forecast': daily_forecast
         }
-    except KeyError:
+    except (KeyError, IndexError):
         return False
 
     return weather_data
@@ -82,42 +70,129 @@ def get_gradient_color(temperature):
     else:
         return "white"
 
+def get_emoji_for_weather_id(weather_id):
+    if weather_id == 113:
+        return ":sun:"
+    elif weather_id == 116:
+        return ":sun_behind_small_cloud:"
+    elif weather_id == 119:
+        return ":cloud:"
+    elif weather_id == 122:
+        return ":cloud:"
+    elif weather_id == 143:
+        return ":fog:"
+    elif weather_id == 176:
+        return ":umbrella_with_rain_drops:"
+    elif weather_id == 179:
+        return ":snowflake:"
+    elif weather_id == 182:
+        return ":snowflake:"
+    elif weather_id == 185:
+        return ":snowflake:"
+    elif weather_id == 200:
+        return ":cloud_with_lightning_and_rain:"
+    elif weather_id == 227:
+        return ":snowflake:"
+    elif weather_id == 230:
+        return ":snowflake:"
+    elif weather_id == 248:
+        return ":fog:"
+    elif weather_id == 260:
+        return ":fog:"
+    elif weather_id == 263:
+        return ":umbrella_with_rain_drops:"
+    elif weather_id == 266:
+        return ":umbrella_with_rain_drops:"
+    elif weather_id == 281:
+        return ":snowflake:"
+    elif weather_id == 284:
+        return ":snowflake:"
+    elif weather_id == 293:
+        return ":umbrella_with_rain_drops:"
+    elif weather_id == 296:
+        return ":umbrella_with_rain_drops:"
+    elif weather_id == 299:
+        return ":umbrella_with_rain_drops:"
+    elif weather_id == 302:
+        return ":umbrella_with_rain_drops:"
+    elif weather_id == 305:
+        return ":umbrella_with_rain_drops:"
+    elif weather_id == 308:
+        return ":umbrella_with_rain_drops:"
+    elif weather_id == 311:
+        return ":snowflake:"
+    elif weather_id == 314:
+        return ":snowflake:"
+    elif weather_id == 317:
+        return ":snowflake:"
+    elif weather_id == 320:
+        return ":snowflake:"
+    elif weather_id == 323:
+        return ":snowflake:"
+    elif weather_id == 326:
+        return ":snowflake:"
+    elif weather_id == 329:
+        return ":snowflake:"
+    elif weather_id == 332:
+        return ":snowflake:"
+    elif weather_id == 335:
+        return ":snowflake:"
+    elif weather_id == 338:
+        return ":snowflake:"
+    elif weather_id == 350:
+        return ":snowflake:"
+    elif weather_id == 353:
+        return ":umbrella_with_rain_drops:"
+    elif weather_id == 356:
+        return ":umbrella_with_rain_drops:"
+    elif weather_id == 359:
+        return ":umbrella_with_rain_drops:"
+    elif weather_id == 362:
+        return ":snowflake:"
+    elif weather_id == 365:
+        return ":snowflake:"
+    elif weather_id == 368:
+        return ":snowflake:"
+    elif weather_id == 371:
+        return ":snowflake:"
+    elif weather_id == 374:
+        return ":snowflake:"
+    elif weather_id == 377:
+        return ":snowflake:"
+    elif weather_id == 386:
+        return ":cloud_with_lightning_and_rain:"
+    elif weather_id == 389:
+        return ":cloud_with_lightning_and_rain:"
+    elif weather_id == 392:
+        return ":cloud_with_lightning_and_rain:"
+    elif weather_id == 395:
+        return ":cloud_with_lightning_and_rain:"
+    else:
+        return ""
+
 def render_wx():
     weather_data = get_wx()
-    emoji_dict = {
-        200: ":cloud_with_lightning_and_rain:", 201: ":cloud_with_lightning_and_rain:", 202: ":cloud_with_lightning_and_rain:", 210: ":cloud_with_lightning_and_rain:", 211: ":cloud_with_lightning_and_rain:", 212: ":cloud_with_lightning_and_rain:", 221: ":cloud_with_lightning_and_rain:", 230: ":cloud_with_lightning_and_rain:",
-        231: ":cloud_with_lightning_and_rain:", 232: ":cloud_with_lightning_and_rain:",
-        300: ":umbrella_with_rain_drops:", 301: ":umbrella_with_rain_drops:", 302: ":umbrella_with_rain_drops:", 310: ":umbrella_with_rain_drops:", 311: ":umbrella_with_rain_drops:",
-        312: ":umbrella_with_rain_drops:", 313: ":umbrella_with_rain_drops:", 314: ":umbrella_with_rain_drops:", 321: ":umbrella_with_rain_drops:",
-        500: ":umbrella_with_rain_drops:", 501: ":umbrella_with_rain_drops:", 502: ":umbrella_with_rain_drops:", 503: ":umbrella_with_rain_drops:", 504: ":umbrella_with_rain_drops:",
-        511: ":umbrella_with_rain_drops:", 520: ":umbrella_with_rain_drops:", 521: ":umbrella_with_rain_drops:", 522: ":umbrella_with_rain_drops:", 531: ":umbrella_with_rain_drops:",
-        600: ":snowflake:", 601: ":snowflake:", 602: ":snowflake:", 611: ":snowflake:", 612: ":snowflake:",
-        613: ":snowflake:", 615: ":snowflake:", 616: ":snowflake:", 620: ":snowflake:", 621: ":snowflake:",
-        622: ":snowflake:",
-        701: ":fog:", 711: ":fog:", 721: ":fog:", 731: ":fog:", 741: ":fog:", 751: ":fog:", 761: ":fog:", 762: ":fog:",
-        771: ":fog:",
-        781: ":cyclone:",
-        800: ":sun:",
-        801: ":sun_behind_small_cloud:", 802: ":sun_behind_large_cloud:", 803: ":cloud:", 804: ":cloud:",
-    }
     tridash = '\n' + '---\n'
-
     if weather_data is False:
         return 'Err' + tridash + 'Could not get weather; Maybe check API key or location?'
 
-    emojiweather = emoji.emojize(emoji_dict[weather_data['id']])
+    emojiweather = emoji.emojize(get_emoji_for_weather_id(weather_data['id']))
 
     color_code = get_gradient_color(weather_data['temperature'])
-    emoji_t = f'{emojiweather}{weather_data["temperature"]}{weather_data["unit"]} | color={color_code}'
+    emoji_t = '{0}{1}{2} | color={3}'.format(emojiweather, weather_data["temperature"], weather_data["unit"], color_code)
     condi = [x.capitalize() for x in weather_data['condition'].split(' ')]
     daily_forecast_encoded = '\nForecast:\n'
     for daily_forecast in weather_data['daily_forecast']:
-        daily_forecast_encoded = f"{daily_forecast_encoded}{daily_forecast['datetime'].strftime('%a')} " \
-                                 f"{daily_forecast['datetime'].month}/{daily_forecast['datetime'].day} " \
-                                 f"{emoji.emojize(emoji_dict[daily_forecast['id']])} " \
-                                 f"{daily_forecast['max']}{weather_data['unit']}/" \
-                                 f"{daily_forecast['min']}{weather_data['unit']}"} | font=Menlo\n"
-    return f'{emoji_t}{tridash}Condition: {" ".join(condi)}{daily_forecast_encoded}'
+        daily_forecast_encoded = "{0}{1} {2}/{3} {4} {5}/{6} | font=Menlo\n".format(
+            daily_forecast_encoded,
+            daily_forecast['datetime'].strftime('%a'),
+            daily_forecast['datetime'].month,
+            daily_forecast['datetime'].day,
+            emoji.emojize(get_emoji_for_weather_id(daily_forecast['id'])),
+            daily_forecast['max'] + weather_data['unit'],
+            daily_forecast['min'] + weather_data['unit']
+        )
+    return '{0}{1}Condition: {2}{3}'.format(emoji_t, tridash, " ".join(condi), daily_forecast_encoded)
 
 
 print(render_wx())
