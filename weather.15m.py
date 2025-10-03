7a43251ea5a5da00fcfbf6f1202b9e224b2e7970
Mike Michon <mmichon@google.com>
2025-10-03 16:45:27 -0400

    Added geolocation.

--- weather.15m.py
+++ weather.15m.py
@@ -1,84 +1,89 @@
 #!/usr/bin/env python3
 
-# <xbar.title>Weather</xbar.title>
-# <xbar.version>v1.4</xbar.version>
-# <xbar.author>Daniel Seripap, Matt Michon</xbar.author>
-# <xbar.author.github>dseripap,mmichon</xbar.author.github>
-# <xbar.desc>Grabs simple weather information from wttr.in.</xbar.desc>
-# <xbar.image>https://i.imgur.com/i1naC4j.png</xbar.image>
-# <xbar.dependencies>python,emoji,requests</xbar.dependencies>
-# <xbar.var>select(VAR_UNITS, options=["imperial", "metric", "kelvin"], default="imperial"): Your preferred units.</xbar.var>
-# <xbar.var>string(VAR_LOCATION, default=""): Your location. If empty, it will be autodetected. You can use a city name, zip code, airport code, landmark, or GPS coordinates (e.g. 48.8582,2.2945).</xbar.var>
+# <xbar.title>Weather - wttr.in</xbar.title>
+# <xbar.version>v2.0</xbar.version>
+# <xbar.author>Daniel Seripap</xbar.author>
+# <xbar.author.github>seripap</xbar.author.github>
+# <xbar.desc>Grabs simple weather information from wttr.in. No API key required.</xbar.desc>
+# <xbar.image>https://poolis.github.io/bitbar-plugins/open-weather-preview.png</xbar.image>
+# <xbar.dependencies>python,emoji</xbar.dependencies>
 
 import emoji
 import json
-import requests
 from urllib.request import urlopen
 from urllib.error import URLError
-
+from random import randint
 import datetime
 import os
+from urllib.parse import quote
 
-units = os.getenv('VAR_UNITS', 'imperial')  # imperial, metric, or kelvin
-try:
-    from CoreLocation import CLLocationManager, kCLAuthorizationStatusAuthorized, kCLLocationAccuracyKilometer
-    import time
-except ImportError:
-    CoreLocation = None
+units = 'imperial'  # 'metric' for Celsius, 'imperial' for Fahrenheit
+lang = 'en'
 
-def get_corelocation():
-    if CoreLocation is None:
+def get_location():
+    try:
+        response = urlopen('http://ipinfo.io/json')
+        data = json.load(response)
+        return data['city']
+    except Exception:
         return None
-
-    manager = CLLocationManager.alloc().init()
-    if manager.authorizationStatus() != kCLAuthorizationStatusAuthorized:
-        return None
-    manager.desiredAccuracy = kCLLocationAccuracyKilometer
-    manager.startUpdatingLocation()
-    while manager.location() is None:
-        time.sleep(0.1)
-    location = manager.location().coordinate()
-    manager.stopUpdatingLocation()
-    return location.latitude, location.longitude
 
 def get_wx():
-    location_name = os.getenv('VAR_LOCATION')
-    if not location_name:
-        location = get_corelocation()
-        if location:
-            location_name = "{},{}".format(location[0], location[1])
-        else:
-            try:
-                geo_info = requests.get('https://ipinfo.io/json').json()
-                location_name = geo_info['city']
-            except (requests.exceptions.RequestException, KeyError):
-                return False
+    city = get_location()
+    if city:
+        city_encoded = quote(city)
+        url = f'http://wttr.in/{city_encoded}?format=j1&lang={lang}'
+    else:
+        url = f'http://wttr.in/?format=j1&lang={lang}'
     try:
-        wx = json.load(urlopen('https://wttr.in/{0}?format=j1'.format(location_name.replace(" ", "%20"))))
-    except URLError:
+        response = urlopen(url)
+        wx = json.load(response)
+    except (URLError, json.JSONDecodeError):
         return False
 
     if units == 'metric':
-        unit = 'C'
-    elif units == 'imperial':
-        unit = 'F'
-    else:
-        unit = 'K'  # Default is kelvin
+        hourly_temp_unit = 'tempC'
+        current_temp_unit = 'temp_C'
+        maxtemp_unit = 'maxtempC'
+        mintemp_unit = 'mintempC'
+        unit_symbol = 'C'
+    else: # imperial
+        hourly_temp_unit = 'tempF'
+        current_temp_unit = 'temp_F'
+        maxtemp_unit = 'maxtempF'
+        mintemp_unit = 'mintempF'
+        unit_symbol = 'F'
 
     try:
+        now = datetime.datetime.now()
+        current_day_forecast = wx['weather'][0] # Today's forecast
+        current_hour_forecast = None
+        for hourly_forecast in reversed(current_day_forecast['hourly']):
+            if int(hourly_forecast['time']) // 100 <= now.hour:
+                current_hour_forecast = hourly_forecast
+                break
+
+        if current_hour_forecast:
+            temp_to_use = current_hour_forecast[hourly_temp_unit]
+        else:
+            # Fallback to current_condition if hourly forecast for current hour is not available
+            temp_to_use = wx['current_condition'][0][current_temp_unit]
+
         daily_forecast = []
         for day in wx['weather']:
-            daily_forecast.append({'id': day['hourly'][0]['weatherCode'],
-                                   'datetime': datetime.datetime.strptime(day['date'], '%Y-%m-%d'),
-                                   'max': str(int(round(float(day['maxtempF'])))) if units == 'imperial' else str(int(round(float(day['maxtempC'])))),
-                                   'min': str(int(round(float(day['mintempF'])))) if units == 'imperial' else str(int(round(float(day['mintempC'])))),
-                                   })
+            daily_forecast.append({
+                'id': int(day['hourly'][0]['weatherCode']),
+                'datetime': datetime.datetime.strptime(day['date'], '%Y-%m-%d'),
+                'max': day[maxtemp_unit],
+                'min': day[mintemp_unit]
+            })
+
         weather_data = {
-            'temperature': str(int(round(float(wx['current_condition'][0]['temp_F'])))) if units == 'imperial' else str(int(round(float(wx['current_condition'][0]['temp_C'])))),
-            'condition': str(wx['current_condition'][0]['weatherDesc'][0]['value']),
+            'temperature': temp_to_use,
+            'condition': wx['current_condition'][0]['weatherDesc'][0]['value'],
             'id': int(wx['current_condition'][0]['weatherCode']),
             'city': wx['nearest_area'][0]['areaName'][0]['value'],
-            'unit': '°' + unit,
+            'unit': '°' + unit_symbol,
             'daily_forecast': daily_forecast
         }
     except (KeyError, IndexError):
@@ -102,129 +107,77 @@ def get_gradient_color(temperature):
     else:
         return "white"
 
-def get_emoji_for_weather_id(weather_id):
-    if weather_id == 113:
-        return ":sun:"
-    elif weather_id == 116:
-        return ":sun_behind_small_cloud:"
-    elif weather_id == 119:
-        return ":cloud:"
-    elif weather_id == 122:
-        return ":cloud:"
-    elif weather_id == 143:
-        return ":fog:"
-    elif weather_id == 176:
-        return ":umbrella_with_rain_drops:"
-    elif weather_id == 179:
-        return ":snowflake:"
-    elif weather_id == 182:
-        return ":snowflake:"
-    elif weather_id == 185:
-        return ":snowflake:"
-    elif weather_id == 200:
-        return ":cloud_with_lightning_and_rain:"
-    elif weather_id == 227:
-        return ":snowflake:"
-    elif weather_id == 230:
-        return ":snowflake:"
-    elif weather_id == 248:
-        return ":fog:"
-    elif weather_id == 260:
-        return ":fog:"
-    elif weather_id == 263:
-        return ":umbrella_with_rain_drops:"
-    elif weather_id == 266:
-        return ":umbrella_with_rain_drops:"
-    elif weather_id == 281:
-        return ":snowflake:"
-    elif weather_id == 284:
-        return ":snowflake:"
-    elif weather_id == 293:
-        return ":umbrella_with_rain_drops:"
-    elif weather_id == 296:
-        return ":umbrella_with_rain_drops:"
-    elif weather_id == 299:
-        return ":umbrella_with_rain_drops:"
-    elif weather_id == 302:
-        return ":umbrella_with_rain_drops:"
-    elif weather_id == 305:
-        return ":umbrella_with_rain_drops:"
-    elif weather_id == 308:
-        return ":umbrella_with_rain_drops:"
-    elif weather_id == 311:
-        return ":snowflake:"
-    elif weather_id == 314:
-        return ":snowflake:"
-    elif weather_id == 317:
-        return ":snowflake:"
-    elif weather_id == 320:
-        return ":snowflake:"
-    elif weather_id == 323:
-        return ":snowflake:"
-    elif weather_id == 326:
-        return ":snowflake:"
-    elif weather_id == 329:
-        return ":snowflake:"
-    elif weather_id == 332:
-        return ":snowflake:"
-    elif weather_id == 335:
-        return ":snowflake:"
-    elif weather_id == 338:
-        return ":snowflake:"
-    elif weather_id == 350:
-        return ":snowflake:"
-    elif weather_id == 353:
-        return ":umbrella_with_rain_drops:"
-    elif weather_id == 356:
-        return ":umbrella_with_rain_drops:"
-    elif weather_id == 359:
-        return ":umbrella_with_rain_drops:"
-    elif weather_id == 362:
-        return ":snowflake:"
-    elif weather_id == 365:
-        return ":snowflake:"
-    elif weather_id == 368:
-        return ":snowflake:"
-    elif weather_id == 371:
-        return ":snowflake:"
-    elif weather_id == 374:
-        return ":snowflake:"
-    elif weather_id == 377:
-        return ":snowflake:"
-    elif weather_id == 386:
-        return ":cloud_with_lightning_and_rain:"
-    elif weather_id == 389:
-        return ":cloud_with_lightning_and_rain:"
-    elif weather_id == 392:
-        return ":cloud_with_lightning_and_rain:"
-    elif weather_id == 395:
-        return ":cloud_with_lightning_and_rain:"
-    else:
-        return ""
-
 def render_wx():
     weather_data = get_wx()
+    # Weather codes from https://www.worldweatheronline.com/weather-api/api/docs/weather-icons.aspx
+    emoji_dict = {
+        113: ":sun:",  # Sunny
+        116: ":sun_behind_small_cloud:",  # Partly cloudy
+        119: ":cloud:",  # Cloudy
+        122: ":cloud:",  # Overcast
+        143: ":fog:",  # Mist
+        176: ":cloud_with_rain:",  # Patchy rain possible
+        179: ":cloud_with_snow:",  # Patchy snow possible
+        182: ":cloud_with_sleet:",  # Patchy sleet possible
+        185: ":cloud_with_rain:",  # Patchy freezing drizzle possible
+        200: ":cloud_with_lightning_and_rain:",  # Thundery outbreaks possible
+        227: ":cloud_with_snow:",  # Blowing snow
+        230: ":snowflake:",  # Blizzard
+        248: ":fog:",  # Fog
+        260: ":fog:",  # Freezing fog
+        263: ":cloud_with_rain:",  # Patchy light drizzle
+        266: ":cloud_with_rain:",  # Light drizzle
+        281: ":cloud_with_rain:",  # Freezing drizzle
+        284: ":cloud_with_rain:",  # Heavy freezing drizzle
+        293: ":cloud_with_rain:",  # Patchy light rain
+        296: ":cloud_with_rain:",  # Light rain
+        299: ":cloud_with_rain:",  # Moderate rain at times
+        302: ":cloud_with_rain:",  # Moderate rain
+        305: ":cloud_with_rain:",  # Heavy rain at times
+        308: ":cloud_with_rain:",  # Heavy rain
+        311: ":cloud_with_rain:",  # Light freezing rain
+        314: ":cloud_with_rain:",  # Moderate or heavy freezing rain
+        317: ":cloud_with_sleet:",  # Light sleet
+        320: ":cloud_with_sleet:",  # Moderate or heavy sleet
+        323: ":cloud_with_snow:",  # Patchy light snow
+        326: ":cloud_with_snow:",  # Light snow
+        329: ":cloud_with_snow:",  # Patchy moderate snow
+        332: ":cloud_with_snow:",  # Moderate snow
+        335: ":cloud_with_snow:",  # Patchy heavy snow
+        338: ":cloud_with_snow:",  # Heavy snow
+        350: ":ice:",  # Ice pellets
+        353: ":cloud_with_rain:",  # Light rain shower
+        356: ":cloud_with_rain:",  # Moderate or heavy rain shower
+        359: ":cloud_with_rain:",  # Torrential rain shower
+        362: ":cloud_with_sleet:",  # Light sleet showers
+        365: ":cloud_with_sleet:",  # Moderate or heavy sleet showers
+        368: ":cloud_with_snow:",  # Light snow showers
+        371: ":cloud_with_snow:",  # Moderate or heavy snow showers
+        374: ":ice:",  # Light showers of ice pellets
+        377: ":ice:",  # Moderate or heavy showers of ice pellets
+        386: ":cloud_with_lightning_and_rain:",  # Patchy light rain with thunder
+        389: ":cloud_with_lightning_and_rain:",  # Moderate or heavy rain with thunder
+        392: ":cloud_with_lightning_and_snow:",  # Patchy light snow with thunder
+        395: ":cloud_with_lightning_and_snow:",  # Moderate or heavy snow with thunder
+    }
     tridash = '\n' + '---\n'
-    if weather_data is False:
-        return 'Err' + tridash + 'Could not get weather; Maybe check API key or location?'
 
-    emojiweather = emoji.emojize(get_emoji_for_weather_id(weather_data['id']))
+    if weather_data is False:
+        return 'Err' + tridash + 'Could not get weather.'
+
+    emojiweather = emoji.emojize(emoji_dict.get(weather_data['id'], ":question_mark:"))
 
     color_code = get_gradient_color(weather_data['temperature'])
-    emoji_t = '{0}{1}{2} | color={3}'.format(emojiweather, weather_data["temperature"], weather_data["unit"], color_code)
+    emoji_t = f'{emojiweather}{weather_data["temperature"]}{weather_data["unit"]} | color={color_code}'
     condi = [x.capitalize() for x in weather_data['condition'].split(' ')]
     daily_forecast_encoded = '\nForecast:\n'
     for daily_forecast in weather_data['daily_forecast']:
-        daily_forecast_encoded = "{0}{1} {2}/{3} {4} {5}/{6} | font=Menlo\n".format(
-            daily_forecast_encoded,
-            daily_forecast['datetime'].strftime('%a'),
-            daily_forecast['datetime'].month,
-            daily_forecast['datetime'].day,
-            emoji.emojize(get_emoji_for_weather_id(daily_forecast['id'])),
-            daily_forecast['max'] + weather_data['unit'],
-            daily_forecast['min'] + weather_data['unit']
-        )
-    return '{0}{1}Condition: {2}{3}'.format(emoji_t, tridash, " ".join(condi), daily_forecast_encoded)
+        daily_forecast_encoded = f"{daily_forecast_encoded}{daily_forecast['datetime'].strftime('%a')} " \
+                                 f"{daily_forecast['datetime'].month}/{daily_forecast['datetime'].day} " \
+                                 f"{emoji.emojize(emoji_dict.get(daily_forecast['id'], ':question_mark:'))} " \
+                                 f"{daily_forecast['max']}{weather_data['unit']}/" \
+                                 f"{daily_forecast['min']}{weather_data['unit']} | font=Menlo\n"
+    return f'{emoji_t}{tridash}Condition: {" ".join(condi)}{daily_forecast_encoded}'
 
 
 print(render_wx())
